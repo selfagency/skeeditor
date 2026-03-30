df07294ea78fd72d5ae241c50a3a3d444dfd8f5f
Daniel Sieradski <daniel@self.agency>
2026-03-29 22:02:54 -0400

    update tasks

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,3 +1,9 @@
 export default {
-  '**/*.{ts,js}': files => [`oxlint --fix ${files.join(' ')}`, `oxfmt ${files.join(' ')} --write`],
+  '**/*.{ts,js}': files => [
+    `./node_modules/.bin/tsc --no-emit`,
+    `./node_modules/.bin/vitest run --project unit`,
+    `./node_modules/.bin/vitest run --project integration`,
+    `./node_modules/.bin/oxlint --fix ${files.join(' ')}`,
+    `./node_modules/.bin/oxfmt --write ${files.join(' ')}`
+  ],
 };
